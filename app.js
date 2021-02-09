9193bb27dac9e94643fd64032efe1d3d0673e02e
Harry <jcceong@gmail.com>
2021-02-09 15:29:04 -0800

    express

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+
+const RSSParser = require('rss-parser');
+parser = new RSSParser();
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-  });
-  
-  server.listen(port, () => {
-    console.log(`Server running at ${port}`);
-  });
+app.get('/', (req, res) =>
+  res.send("Hello World!")
+);
